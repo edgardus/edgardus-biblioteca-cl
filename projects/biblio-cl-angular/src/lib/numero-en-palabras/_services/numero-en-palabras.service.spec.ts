08b490260db70d571d1faadf47c059fdb3b711c3
Edgardo Olivares <edgardus@gmail.com>
2020-04-18 03:16:25 -0400

    avance

--- projects/biblio-cl-angular/src/lib/numero-en-palabras/_services/numero-en-palabras.service.spec.ts
+++ projects/biblio-cl-angular/src/lib/numero-en-palabras/_services/numero-en-palabras.service.spec.ts
@@ -4,19 +4,25 @@ import { NumeroEnPalabrasService } from './numero-en-palabras.service';
 
 describe('NumeroEnPalabrasService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
+  // const listaValores = [ { valor: 112122, texto: 'ciento doce mil ciento veintidós' } ];
   const listaValores = [
     { valor: 101, texto: 'ciento uno' },
     { valor: 32, texto: 'treinta y dos' },
     { valor: 132, texto: 'ciento treinta y dos' },
     { valor: 1132, texto: 'mil ciento treinta y dos' },
     { valor: 2132, texto: 'dos mil ciento treinta y dos' },
-    { valor: 102, texto: 'ciento dos' }
+    { valor: 102, texto: 'ciento dos' },
+    { valor: 2002, texto: 'dos mil dos' },
+    { valor: 2022, texto: 'dos mil veintidós' },
+    { valor: 12022, texto: 'doce mil veintidós' },
+    { valor: 12122, texto: 'doce mil ciento veintidós' },
+    { valor: 112122, texto: 'ciento doce mil ciento veintidós' }
   ];
 
   listaValores.forEach(element => {
     it('Test ' + element.valor, () => {
       const service: NumeroEnPalabrasService = TestBed.get(NumeroEnPalabrasService);
-      console.log('test ' + element.valor + ':' + service.numeroEnPalabras(element.valor));
+      console.log('*******  test ' + element.valor + ':' + service.numeroEnPalabras(element.valor));
       expect(service.numeroEnPalabras(element.valor)).toEqual(element.texto);
     });
   });
--- projects/biblio-cl-angular/src/lib/numero-en-palabras/_services/numero-en-palabras.service.ts
+++ projects/biblio-cl-angular/src/lib/numero-en-palabras/_services/numero-en-palabras.service.ts
@@ -12,22 +12,55 @@ export class NumeroEnPalabrasService {
   /**
    *
    * @param x Numero a ser expresado en palabras.
+   * @param textoAntes Texto previo
+   * @param textoDespues Texto post
+   * @returns :string
+   *
    */
-  numeroEnPalabras(x: number, prefix: string = ''): string {
+  numeroEnPalabras(x: number, textoAntes: string = '', textoDespues: string = ''): string {
     let textus = '';
     let numero = x;
     const valorExacto = VALORES_NUMEROS.find(vn => vn.valor === numero);
     if (noEsNulo(valorExacto)) {
-      textus = prefix + valorExacto.texto;
+      textus = textoAntes + valorExacto.texto;
       return textus;
     }
+    let numeroStr = numero.toString();
+    if (numeroStr.length >= 6) {
+      const grupoValores = [];
+      // modifica str.slice(3, -2); extrae str.substr(-2,2)
+      while (numeroStr.length > 0) {
+        grupoValores.push(numeroStr.substr(-3, 3));
+        numeroStr = numeroStr.slice(0, -3);
+      }
+
+    }
+
     const rango = VALORES_NUMEROS.filter(ra => (ra.valor < x) && ra.valor.toString().endsWith('0'))
       .reduce((a, b) => (a.valor > b.valor) ? a : b);
     if (noEsNulo(rango)) {
       const txt = (rango.prefijo.trim() === '' ? rango.texto : rango.prefijo) + ' ';
-      numero = numero - rango.valor;
-      textus = prefix + (numero > 0 ? txt : rango.texto);
-      textus = rango.valor < 100 ? textus + 'y ' : textus;
+      const numeroNuevo = numero - rango.valor;
+      if (numeroNuevo > 1000) {
+        const valorY = (rango.valor / 10);
+        const resto = numero % valorY;
+        console.log('valorY' + valorY);
+        const valorX = (numero - resto) / valorY;
+        console.log('valorX' + valorX);
+        const nnvalorExacto = VALORES_NUMEROS.find(vn => vn.valor === valorX);
+        const nnvalorExactoY = VALORES_NUMEROS.find(vn => vn.valor === valorY);
+        if (noEsNulo(nnvalorExacto)) {
+          textus = textoAntes + nnvalorExacto.texto + ' ' + nnvalorExactoY.texto + ' ';
+          numero = resto;
+        } else {
+
+          return 'err';
+        }
+      } else {
+        numero = numeroNuevo;
+        textus = textoAntes + (numeroNuevo > 0 ? txt : rango.texto);
+        textus = rango.valor < 100 ? textus + 'y ' : textus;
+      }
       return numero > 0 ? this.numeroEnPalabras(numero, textus) : textus;
     }
     return null;
